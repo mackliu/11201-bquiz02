fc814fc8320e32c094b47289df3cbba4072e0082
mackliu <macklun@gmail.com>
2023-07-21 15:24:53 +0800

    撰寫取得文章列表程式

--- view/front/po.php
+++ view/front/po.php
@@ -4,19 +4,31 @@
 <div style="display:flex">
 <fieldset style="width:150px;">
     <legend>分類網誌</legend>
-<div><a class='cat' href="#">健康新知</a></div>
-<div><a class='cat' href="#">菸害防治</a></div>
-<div><a class='cat' href="#">癌症防治</a></div>
-<div><a class='cat' href="#">慢性病防治</a></div>
+<div><a class='cat' data-type='1' href="#">健康新知</a></div>
+<div><a class='cat' data-type='2' href="#">菸害防治</a></div>
+<div><a class='cat' data-type='3' href="#">癌症防治</a></div>
+<div><a class='cat' data-type='4' href="#">慢性病防治</a></div>
 </fieldset>
 <fieldset style="width:550px;">
     <legend>文章列表</legend>
+    <div id="lists"></div>
+    <div id="post"></div>
 </fieldset>
 </div>
 <script>
+
+getList(1);
+
 $(".cat").on("click",function(){
     $("#header").text($(this).text())
+    let type=$(this).data('type')
+    getList(type)
 })
 
 
+function getList(type){
+    $.get("./api/get_list.php",{type},(list)=>{
+        $("#lists").html(list)
+    })
+}
 </script>
