e3a58a425ecedd0e3f519c0942fa760a983e028a
mackliu <macklun@gmail.com>
2023-07-21 15:10:13 +0800

    建立分類網誌頁面及nav文字替換功能

---
+++ view/front/po.php
@@ -0,0 +1,22 @@
+<div>
+    目前位置：首頁 > 分類網誌 > <span id='header'>健康新知</span>
+</div>
+<div style="display:flex">
+<fieldset style="width:150px;">
+    <legend>分類網誌</legend>
+<div><a class='cat' href="#">健康新知</a></div>
+<div><a class='cat' href="#">菸害防治</a></div>
+<div><a class='cat' href="#">癌症防治</a></div>
+<div><a class='cat' href="#">慢性病防治</a></div>
+</fieldset>
+<fieldset style="width:550px;">
+    <legend>文章列表</legend>
+</fieldset>
+</div>
+<script>
+$(".cat").on("click",function(){
+    $("#header").text($(this).text())
+})
+
+
+</script>
